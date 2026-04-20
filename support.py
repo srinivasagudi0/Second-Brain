487da9703ea4ad40f986b3c81c194ae69a8cdc8b
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-04-20 12:43:18 -0500

    added initializing db and saving notes.
    I will call it in app.py

--- support.py
+++ support.py
@@ -1,1 +1,21 @@
 # all the supporting funcrtions stay here
+
+import sqlite3
+
+db = "notes.db"
+
+# we will be creating multiple dbs, so it is easy; this note db will be used for storing notes, we can have another db for tasks, and so on. This way we can keep things organized and modular.
+def init_db():
+    conn = sqlite3.connect(db)
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS notes
+                 (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT)''')
+    conn.commit()
+    conn.close()
+
+def save_note_to_db(note):
+    conn = sqlite3.connect(db)
+    c = conn.cursor()
+    c.execute("INSERT INTO notes (content) VALUES (?)", (note,))
+    conn.commit()
+    conn.close()
